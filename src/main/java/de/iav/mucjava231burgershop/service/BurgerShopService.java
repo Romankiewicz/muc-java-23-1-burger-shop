bc690b74210a70492c5e545b9d702a57d5992d60
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-06-01 12:15:42 +0200

    build Service constructor and listMenus method added

--- src/main/java/de/iav/mucjava231burgershop/service/BurgerShopService.java
+++ src/main/java/de/iav/mucjava231burgershop/service/BurgerShopService.java
@@ -1,8 +1,21 @@
 package de.iav.mucjava231burgershop.service;
 
+import de.iav.mucjava231burgershop.model.Menu;
+import de.iav.mucjava231burgershop.repository.MenuRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BurgerShopService {
 
+    private final MenuRepository menuRepository;
+
+    public BurgerShopService(MenuRepository menuRepository) {
+        this.menuRepository = menuRepository;
+    }
+
+    public List<Menu> listMenus() {
+        return menuRepository.menuList();
+    }
 }
