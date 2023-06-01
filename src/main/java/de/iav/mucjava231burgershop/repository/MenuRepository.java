eecce044be53be1b50328985d07e48c940222e5a
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-06-01 10:32:54 +0200

    create MenuRepository + add methods

---
+++ src/main/java/de/iav/mucjava231burgershop/repository/MenuRepository.java
@@ -0,0 +1,34 @@
+package de.iav.mucjava231burgershop.repository;
+
+import de.iav.mucjava231burgershop.exception.MenuNotFoundException;
+import de.iav.mucjava231burgershop.model.Menu;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class MenuRepository {
+
+    private List<Menu> menus;
+
+    public MenuRepository(){
+        menus = new ArrayList<>();
+    }
+    public MenuRepository(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public List<Menu> menuList(){
+        return menus;
+    }
+
+    public Menu getMenuById(String idOfRequestedMenu){
+        for (Menu singleMenuFromList : menus){
+            if(singleMenuFromList.id().equals(idOfRequestedMenu)) {
+                return singleMenuFromList;
+            }
+        }
+        throw new MenuNotFoundException(idOfRequestedMenu);
+    }
+
+}
